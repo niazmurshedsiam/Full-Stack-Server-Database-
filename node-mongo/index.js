595faa3e71bed7e28fde17a103cad074caf50170
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-04 05:47:48 +0600

    how to publish node project in git

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,8 +1,6 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('hello world')
-  })
-  
-  app.listen(8000,()=>{console.log('Listenting to port 8000')}) ;
+app.get('/',(req,res)=>{res.send('Hello')})
+
+app.listen(8000,()=>{console.log('listen to port 8000')});
