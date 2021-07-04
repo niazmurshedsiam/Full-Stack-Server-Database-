2389abb597964ec4655006ebe5e5999249f48207
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-04 06:16:33 +0600

    dynamic api, api parameter, access params

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-
+const users = ['Abir','Karim','Abul','Aslam'];
 app.get('/',(req,res)=>{
     const fruits = {
         name:'Mango',
@@ -11,6 +11,13 @@ app.get('/',(req,res)=>{
 
 app.get('/fruits/mango',(req,res)=>{
     res.send({name:'Mango',quantity:20,price:200})
+})
+
+app.get('/user/:id',(req,res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const name = users[id];
+    res.send({id: name});
 })
 
 app.listen(8000,()=>{console.log('listen to port 8000')});
