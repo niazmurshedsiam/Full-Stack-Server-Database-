7ef485ce898bca8f5ced09ae498239ad876882e9
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-04 06:00:39 +0600

    install nodemon, api return object, create multiple api

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res)=>{res.send('Hello')})
+app.get('/',(req,res)=>{
+    const fruits = {
+        name:'Mango',
+        price:'250'
+    } 
+    res.send(fruits);
+})
+
+app.get('/fruits/mango',(req,res)=>{
+    res.send({name:'Mango',quantity:20,price:200})
+})
 
 app.listen(8000,()=>{console.log('listen to port 8000')});
