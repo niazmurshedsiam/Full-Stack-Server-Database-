95e8a8e1bf6195ae5796b50b6961b34f11e02ff7
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-04 07:06:31 +0600

    use fetch to load data from server, middleware, handle cors

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 const users = ['Abir','Karim','Abul','Aslam'];
 app.get('/',(req,res)=>{
     const fruits = {
@@ -15,9 +17,8 @@ app.get('/fruits/mango',(req,res)=>{
 
 app.get('/user/:id',(req,res)=>{
     const id = req.params.id;
-    console.log(id);
     const name = users[id];
-    res.send({id: name});
+    res.send({id,name})
 })
 
 app.listen(8000,()=>{console.log('listen to port 8000')});
