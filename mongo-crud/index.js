0ffa836f78d006353e4206afd46a4e2f9e69207b
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-08 16:32:23 +0600

    Send data to database

--- mongo-crud/index.js
+++ mongo-crud/index.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://MyDbUser:Tx9W5hHTVvxGSTTY@cluster0.bm8mo.mongodb.net/myDbUser?retryWrites=true&w=majority";
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://organicUser:XZSU0qCXkXDvY0eP@cluster0.bm8mo.mongodb.net/myDbUser?retryWrites=true&w=majority";
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.get('/',(req,res)=>{
-    res.send('hello');
+  res.sendFile(__dirname + '/index.html');
 })
-
 client.connect(err => {
-  const collection = client.db("myDbUser").collection("user");
-  console.log('DataBase Connected');
+  const collection = client.db("myDbUser").collection("products");
+  collection.insertOne({name:"Alu",price:24,quantity:2})
+  .then(res=>{
+    console.log('Add Databsae data');
+  })
+  console.log('connent');
   // perform actions on the collection object
-  client.close();
+  // client.close();
 });
-
-
 app.listen(8000,()=>console.log('listen start port 8080'));
