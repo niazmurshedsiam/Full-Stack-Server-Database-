db224c6522f0a89efeffd76ff92032182b5a7550
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-08 23:13:40 +0600

    Save data in database and Read all data

--- mongo-crud/index.js
+++ mongo-crud/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const app = express();
+
+const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 const uri = "mongodb+srv://organicUser:XZSU0qCXkXDvY0eP@cluster0.bm8mo.mongodb.net/myDbUser?retryWrites=true&w=majority";
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 app.get('/',(req,res)=>{
   res.sendFile(__dirname + '/index.html');
 })
@@ -12,10 +15,14 @@ client.connect(err => {
   const collection = client.db("myDbUser").collection("products");
   
   app.post('/addProduct',(req,res)=>{
-    collection.insertOne({name:"Alu",price:24,quantity:2})
-    .then(res=>{
-      console.log('Add Databsae data');
+    const product = req.body;
+    collection.insertOne(product)
+    .then(result=>{
+      console.log("Data Add Database");
+      res.send('success');
     })
+    
+    
 
   })
   
