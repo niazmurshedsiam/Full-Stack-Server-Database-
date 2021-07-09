768c6d9b116523e22308cb72df6efc6a47ef0642
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-09 06:07:06 +0600

    Delete an item from the database from UI

--- mongo-crud/index.js
+++ mongo-crud/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
+const objectId = require('mongodb').ObjectID;
 const uri = "mongodb+srv://organicUser:XZSU0qCXkXDvY0eP@cluster0.bm8mo.mongodb.net/myDbUser?retryWrites=true&w=majority";
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,6 +19,15 @@ client.connect(err => {
       res.send(document);
     })
 
+  })
+
+  app.delete('/delete/:id',(req,res)=>{
+    collection.deleteOne({_id: ObjectID(req.params.id)})
+    .then(result=>{
+      console.log(result);
+
+    })
+    
   })
   app.post('/addproduct',(req,res)=>{
     const product = req.body;
