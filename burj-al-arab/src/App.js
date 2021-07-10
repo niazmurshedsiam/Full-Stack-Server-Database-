3ff44984552c519d3dd646322429eda91f467258
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-10 21:26:25 +0600

    bruj-al-arab

---
+++ burj-al-arab/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Book from './components/Book/Book';
+import Header from './components/Header/Header';
+import { createContext } from 'react';
+import { useState } from 'react';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+export const UserContext = createContext();
+function App() {
+ const [loginInUser,setLoginInUser] = useState({});
+ 
+  return (
+    <UserContext.Provider value={[loginInUser,setLoginInUser]}>
+      <p>Name: {loginInUser.name}</p>
+      <Router>
+          <Header/>
+          <Switch>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute path="/book/:bedType">
+              <Book />
+            </PrivateRoute>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+      </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ burj-al-arab/src/components/Login/Login.js
@@ -0,0 +1,41 @@
+import React  from 'react';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from '../../firebase.config';
+import { useContext } from 'react';
+import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router-dom';
+
+firebase.initializeApp(firebaseConfig);
+
+const Login = () => {
+    const [loginInUser,setLoginnUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
+    const handleGoogleSignIn = ()=>{
+        const googleprovider = new firebase.auth.GoogleAuthProvider();
+        
+        firebase.auth().signInWithPopup(googleprovider)
+        .then((result) => {
+            const {displayName,email}= result.user;
+            const signInUser = {name: displayName,email};
+            setLoginnUser(signInUser);
+            history.replace(from);
+            console.log(signInUser);
+        }).catch((error) => {
+    
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode,errorMessage);
+  });
+    }
+    
+    return (
+        <div>
+            <button onClick={handleGoogleSignIn}>Google Sign</button>
+        </div>
+    );
+};
+
+export default Login;
