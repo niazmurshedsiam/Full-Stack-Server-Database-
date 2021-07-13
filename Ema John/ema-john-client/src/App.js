fa8e66860e5d648228c3f4b036470569b14b4a32
niazmurshedsiam <niazmurshed16@gmail.com>
2021-07-14 01:32:25 +0600

    create server and client side folder

---
+++ Ema John/ema-john-client/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory';
+import NoMatch from './components/NoMatch/NoMatch';
+import ProductDetail from './components/ProductDetail/ProductDetail';
+import Review from './components/Review/Review';
+import Shop from './components/Shop/Shop';
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Router>
+        <Switch>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <Review></Review>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/product/:productKey">
+            <ProductDetail></ProductDetail>
+          </Route>
+          <Route path="*">
+             <NoMatch></NoMatch>
+          </Route>
+        </Switch>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
