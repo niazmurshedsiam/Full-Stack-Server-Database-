e59bf62d34e94d1d4c5a5c08d5835dba2c64a189
niazmurshedsiam <niazmurshed16@gmail.com>
2021-08-29 22:55:13 +0600

    Connect database server

--- Volunteer Network/Server/index.js
+++ Volunteer Network/Server/index.js
@@ -1,10 +1,28 @@
 const express = require('express')
 const app = express()
-const port = 5000
-
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+const port = process.env.port || 5000;
+// console.log(process.env.DB_NAME);
+app.use(cors());
+app.use(bodyParser.json());
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+const { MongoClient } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm8mo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  console.log('Conection Error',err);
+  const eventsCollection = client.db("VolunteerNetwork").collection("events");
+  console.log('Database Connection');
+  // client.close();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening ${port}`)
