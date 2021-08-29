61b630c8eb6c13f59c1b2ded8bf4bbb0b8c97206
niazmurshedsiam <niazmurshed16@gmail.com>
2021-08-29 22:55:52 +0600

    implement react hook form

--- Volunteer Network/client/src/component/AddEvent/AddEvent.js
+++ Volunteer Network/client/src/component/AddEvent/AddEvent.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
 const AddEvent = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => console.log(data);
     return (
         <div>
-            <h1>This is</h1>
+            <form onSubmit={handleSubmit(onSubmit)}>
+     
+                <input defaultValue="test" {...register("example")} />
+                <br />
+                <input {...register("exampleRequired", { required: true })} />
+                <br />
+                <input type="submit" />
+            </form>
         </div>
     );
 };
