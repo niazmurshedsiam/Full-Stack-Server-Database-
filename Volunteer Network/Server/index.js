764e38c0863232ad58016f9205e146b82b699c64
niazmurshedsiam <niazmurshed16@gmail.com>
2021-08-30 22:18:41 +0600

    Upload events with image link to database

--- Volunteer Network/Server/index.js
+++ Volunteer Network/Server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
-const port = process.env.port || 5000;
+const port = process.env.port || 5001;
 // console.log(process.env.DB_NAME);
 app.use(cors());
 app.use(bodyParser.json());
@@ -15,11 +15,20 @@ app.get('/', (req, res) => {
 
 const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm8mo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  console.log('Conection Error',err);
+  // console.log('Conection Error',err);
   const eventsCollection = client.db("VolunteerNetwork").collection("events");
+  app.post('/addEvent',(req,res)=>{
+    const newEvent = req.body;
+    // console.log('Add by new event: ', newEvent);
+    eventsCollection.insertOne(newEvent)
+    .then(result =>{
+      console.log('inserted count', result.insertedCount);
+      res.send(result.insertedCount > 0)
+    })
+  })
   console.log('Database Connection');
   // client.close();
 });
