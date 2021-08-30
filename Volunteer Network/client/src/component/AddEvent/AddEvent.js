9d1e9e4085ecb16b870409d48edf7c7151210348
niazmurshedsiam <niazmurshed16@gmail.com>
2021-08-30 22:15:39 +0600

    Upload events with image link to database

--- Volunteer Network/client/src/component/AddEvent/AddEvent.js
+++ Volunteer Network/client/src/component/AddEvent/AddEvent.js
@@ -1,31 +1,50 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
 const AddEvent = () => {
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+    const [imageURL,setImageURL] = useState(null);
+    const onSubmit = data => {
+        const eventData = {
+            name: data.name,
+            imageURL: imageURL
+        };
+        const url = `http://localhost:5001/addEvent`;
+      console.log(eventData);
+      fetch(url,{
+          method: 'POST',
+          headers: {
+              'content-type':'application/json'
+          },
+          body: JSON.stringify(eventData)
+      })
+      .then(res=>console.log('server side response',res))
+      
+    };
+
   const handleImageUpload = event =>{
-      console.log(event.target.files[0]);
-      const imageData = new FormData();
-      imageData.set('key','335045a2e20dfb20b2e75072ff553f1f');
-      imageData.append('image',event.target.files[0]);
-      axios.post('https://api.imgbb.com/1/upload',imageData)
+    // console.log(event.target.files[0]);
+    const imageData = new FormData();
+    imageData.set('key','335045a2e20dfb20b2e75072ff553f1f');
+    imageData.append('image',event.target.files[0]);
+    axios.post('https://api.imgbb.com/1/upload',
+    imageData)
       .then(function (response) {
-        console.log(response.data.data.display_url);
+        setImageURL(response.data.data.display_url);
       })
       .catch(function (error) {
         console.log(error);
       });
   }
     return (
-        <div>
+        <div style={{margin:"10px"}}>
             <form onSubmit={handleSubmit(onSubmit)}>
      
-                <input defaultValue="test" {...register("example")} />
+                <input name="name" defaultValue="New exciting event" {...register("example")}/>
                 <br />
-                <input type = "file" onChange={handleImageUpload} />
+                <input type="file" name="exampleRequired" onChange={handleImageUpload}/>
                 <br />
-                <input type="submit" />
+                <input type="submit"/>
             </form>
         </div>
     );
