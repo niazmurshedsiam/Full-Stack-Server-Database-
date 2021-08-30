e059d52ac0e9025c4ff59cf1f2b7ea1ba9801a88
niazmurshedsiam <niazmurshed16@gmail.com>
2021-08-31 01:27:04 +0600

    Display hosted images in the client side and delete event

--- Volunteer Network/Server/index.js
+++ Volunteer Network/Server/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config()
-const port = process.env.port || 5001;
+const port = process.env.port || 5000;
 // console.log(process.env.DB_NAME);
 app.use(cors());
 app.use(bodyParser.json());
@@ -12,14 +14,26 @@ app.get('/', (req, res) => {
 })
 
 
-
-const { MongoClient } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm8mo.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   // console.log('Conection Error',err);
   const eventsCollection = client.db("VolunteerNetwork").collection("events");
+  
+  app.get('/events', (req, res) => {
+        eventCollection.find()
+        .toArray((err, items) => {
+            res.send(items)
+        })
+    })
+    app.delete('deleteEvent/:id', (req, res) => {
+      const id = ObjectID(req.params.id);
+      console.log('delete this', id);
+      eventCollection.findOneAndDelete({_id: id})
+      .then(documents => res.send(!!documents.value))
+  })
+  
   app.post('/addEvent',(req,res)=>{
     const newEvent = req.body;
     // console.log('Add by new event: ', newEvent);
@@ -31,6 +45,8 @@ client.connect(err => {
   })
   console.log('Database Connection');
   // client.close();
+
+
 });
 
 app.listen(port, () => {
